d35f694f5d9b412d8df3e3c266ad5e5b90cb4383
Luis Carlo Hernandez Torres <87973177+kasldkasd@users.noreply.github.com>
2026-05-16 16:28:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
   res.send("Awita de Sandia está despierta.");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🌐 Servidor web activo en puerto ${PORT}`);
 });
 
